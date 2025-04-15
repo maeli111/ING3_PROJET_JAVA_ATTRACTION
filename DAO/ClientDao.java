25e6ba6dd12214e48a1a34fe99ea95f5bffef859
Marie Labous-Filly <marie.labousfilly@edu.ece.fr>
2025-04-15 16:42:41 +0200

    modif

--- DAO/ClientDao.java
+++ DAO/ClientDao.java
@@ -1,56 +1,45 @@
 package DAO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import Modele.Client;
 
 public class ClientDao {
     private DaoFactory daoFactory;
 
-    // constructeur dépendant de la classe DaoFactory
+    // Constructeur
     public ClientDao(DaoFactory daoFactory) {
         this.daoFactory = daoFactory;
     }
 
     /**
      * Récupérer de la base de données tous les objets des clients dans une liste
-     * @return : liste retournée des objets des clients récupérés
+     * @return liste des clients
      */
     public ArrayList<Client> getAll() {
-        ArrayList<Client> listeClients = new ArrayList<Client>();
+        ArrayList<Client> listeClients = new ArrayList<>();
 
-        /*
-            Récupérer la liste des clients de la base de données dans listeClients
-        */
         try {
-            // connexion
-            Connection connexion = daoFactory.getConnection();;
+            Connection connexion = daoFactory.getConnection();
             Statement statement = connexion.createStatement();
 
-            // récupération des produits de la base de données avec la requete SELECT
-            ResultSet resultats = statement.executeQuery("select * from client");
+            // Exécuter la requête SELECT
+            ResultSet resultats = statement.executeQuery("SELECT * FROM client");
 
-            // 	Se déplacer sur le prochain enregistrement : retourne false si la fin est atteinte
             while (resultats.next()) {
-                // récupérer les 3 champs de la table produits dans la base de données
-                int id_client = resultats.getInt(1);
-                int id_utilisateur = resultats.getInt(2);
-                int age = resultats.getInt(3);
-                String type_client = resultats.getString(4);
-                String type_membre = resultats.getString(5);
+                int id_client = resultats.getInt("id_client");
+                int id_utilisateur = resultats.getInt("id_utilisateur");
+                int age = resultats.getInt("age");
+                String type_client = resultats.getString("type_client");
+                String type_membre = resultats.getString("type_membre");
 
-                // instancier un objet de Produit avec ces 3 champs en paramètres
-                Client client = new Client(id_client,id_utilisateur,age,type_client,type_membre);
+                // Utilise le constructeur secondaire simplifié
+                Client client = new Client(id_client, id_utilisateur, age, type_client, type_membre);
 
-                // ajouter ce produit à listeProduits
                 listeClients.add(client);
             }
-        }
-        catch (SQLException e) {
-            //traitement de l'exception
+
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Création de la liste de clients impossible");
         }
@@ -58,5 +47,51 @@ public class ClientDao {
         return listeClients;
     }
 
+    /**
+     * Ajouter un nouveau client dans la base de données
+     */
+    public void ajouter(Client client) {
+        try {
+            Connection connexion = daoFactory.getConnection();
 
+            // 1. Ajouter l'utilisateur
+            String sqlUtilisateur = "INSERT INTO utilisateur (email, nom, prenom, mdp) VALUES (?, ?, ?, ?)";
+            PreparedStatement psUtilisateur = connexion.prepareStatement(sqlUtilisateur, Statement.RETURN_GENERATED_KEYS);
+            psUtilisateur.setString(1, client.getEmail());
+            psUtilisateur.setString(2, client.getNom());
+            psUtilisateur.setString(3, client.getPrenom());
+            psUtilisateur.setString(4, client.getMdp());
+            int lignesUtilisateur = psUtilisateur.executeUpdate();
+
+            if (lignesUtilisateur > 0) {
+                ResultSet rsUtilisateur = psUtilisateur.getGeneratedKeys();
+                if (rsUtilisateur.next()) {
+                    int idUtilisateurGenere = rsUtilisateur.getInt(1);
+                    client.setid_utilisateur(idUtilisateurGenere);
+
+                    // 2. Ajouter le client avec l'ID utilisateur
+                    String sqlClient = "INSERT INTO client (id_utilisateur, age, type_client, type_membre) VALUES (?, ?, ?, ?)";
+                    PreparedStatement psClient = connexion.prepareStatement(sqlClient, Statement.RETURN_GENERATED_KEYS);
+                    psClient.setInt(1, client.getid_utilisateur());
+                    psClient.setInt(2, client.getage());
+                    psClient.setString(3, client.getType_client());
+                    psClient.setString(4, client.getType_membre());
+
+                    int lignesClient = psClient.executeUpdate();
+                    if (lignesClient > 0) {
+                        ResultSet rsClient = psClient.getGeneratedKeys();
+                        if (rsClient.next()) {
+                            int idClientGenere = rsClient.getInt(1);
+                            client.setid_client(idClientGenere);
+                            System.out.println("Client ajouté avec ID : " + idClientGenere);
+                        }
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur lors de l'ajout du client et de l'utilisateur");
+        }
+    }
 }
--- Modele/Utilisateur.java
+++ Modele/Utilisateur.java
@@ -1,18 +1,18 @@
 package Modele;
 
-public class Utilisateur {
+public abstract class Utilisateur {
     private int id_utilisateur;
     private String email;
     private String nom;
     private String prenom;
     private String mdp;
 
-    public Utilisateur(int id_utilisateur,String email, String nom, String prenom, String mdp){
+    public Utilisateur(int id_utilisateur, String email, String nom, String prenom, String mdp) {
         this.id_utilisateur = id_utilisateur;
-        this.email=email;
-        this.nom=nom;
-        this.prenom=prenom;
-        this.mdp=mdp;
+        this.email = email;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mdp = mdp;
     }
 
     public int getId_utilisateur(){return id_utilisateur;}
@@ -20,4 +20,7 @@ public class Utilisateur {
     public String getNom(){return nom;}
     public String getPrenom(){return prenom;}
     public String getMdp(){return mdp;}
+    public void setid_utilisateur(int id_utilisateur) {
+        this.id_utilisateur= id_utilisateur;
+    }
 }
