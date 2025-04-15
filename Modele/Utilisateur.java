8e80f0a2e8b866e7d26ac9981b418961b3d046c7
Marie Labous-Filly <marie.labousfilly@edu.ece.fr>
2025-04-15 14:36:56 +0200

    Création liste Utilisateur et reservation avec bdd

---
+++ Modele/Utilisateur.java
@@ -0,0 +1,23 @@
+package Modele;
+
+public class Utilisateur {
+    private int id_utilisateur;
+    private String email;
+    private String nom;
+    private String prenom;
+    private String mdp;
+
+    public Utilisateur(int id_utilisateur,String email, String nom, String prenom, String mdp){
+        this.id_utilisateur = id_utilisateur;
+        this.email=email;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.mdp=mdp;
+    }
+
+    public int getId_utilisateur(){return id_utilisateur;}
+    public String getEmail(){return email;}
+    public String getNom(){return nom;}
+    public String getPrenom(){return prenom;}
+    public String getMdp(){return mdp;}
+}
