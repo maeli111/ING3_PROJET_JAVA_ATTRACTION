fe39d962ad8e9fadd2756d2de77e43eb21023b66
Marie Labous-Filly <marie.labousfilly@edu.ece.fr>
2025-04-16 17:58:18 +0200

    ajout sources

--- Controleur/main.java
+++ Controleur/main.java
@@ -1,3 +1,7 @@
+//Sources:
+//PreparedStatement -> https://docs.oracle.com/javase/8/docs/api/java/sql/PreparedStatement.html
+//ResultSet -> https://docs.oracle.com/javase/8/docs/api/java/sql/ResultSet.html
+
 package Controleur;
 
 public class main {
